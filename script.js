6bd340f9922acc2bfd643f5dee828a2d7c2db3ed
uexle <wesleivianna235@gmai.com>
2024-03-22 17:44:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,1 @@
+window.location.replace("mailto:marcionmiranda@ufrrj.br?subject=Antecipa%C3%A7%C3%A3o%20da%20prova%20de%20Redes&body=Eu%20concordo%20com%20a%20antecipa%C3%A7%C3%A3o%20da%20prova%20de%20Redes%20para%20o%20dia%2002%2F04%2F2024.");

6e56c68a98d9c65450adc5c697e058f9c461218d
uexle <wesleivianna235@gmai.com>
2024-03-22 19:10:02 -0300

    form

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-window.location.replace("mailto:marcionmiranda@ufrrj.br?subject=Antecipa%C3%A7%C3%A3o%20da%20prova%20de%20Redes&body=Eu%20concordo%20com%20a%20antecipa%C3%A7%C3%A3o%20da%20prova%20de%20Redes%20para%20o%20dia%2002%2F04%2F2024.");
+window.onload = function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const email = urlParams.get('email');
+    const subject = urlParams.get('subject');
+    const body = urlParams.get('body');
+
+    if (email && subject && body) {
+        const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+        window.location.href = mailtoLink;
+    } else {
+        console.error('Missing parameters: email, subject, or body.');
+    }
+}
